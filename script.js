8c6b4f5e8f08ae8e133b4a8d8ebec32a27f85303
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-10 16:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
 	let myElm = document.createElement("p");
 	myElm.innerText = 'DOM load sucess';
 });
